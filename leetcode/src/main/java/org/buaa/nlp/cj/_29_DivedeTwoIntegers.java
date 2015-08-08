5c0bf5a9d1b86cbb876992f20639620594e500c2
wesky260643 <260643431@qq.com>
2015-08-08 14:25:25 +0800

    vvv

--- leetcode/src/main/java/org/buaa/nlp/cj/_29_DivedeTwoIntegers.java
+++ leetcode/src/main/java/org/buaa/nlp/cj/_29_DivedeTwoIntegers.java
@@ -7,18 +7,16 @@ public class _29_DivedeTwoIntegers {
     public int divide(int dividend, int divisor) {
         if (divisor == 0)
             return Integer.MAX_VALUE;
+        if (divisor == dividend)
+            return 1;
 
-        int result = 0;
-        if(dividend==Integer.MIN_VALUE)
-        {
-            result = 1;
-            dividend += Math.abs(divisor);
-        }
-        if(divisor==Integer.MIN_VALUE)
-            return result;
+        long result = 0;
 
-        long cpDividend = Math.abs(dividend);
-        long cpDivisor = Math.abs(divisor);
+        long cpDividend = Math.abs((long)dividend);
+        long cpDivisor = Math.abs((long)divisor);
+
+//        if (cpDividend < cpDivisor)
+//            return 0;
 
         long c = 1;
         // 先将除数每次乘以2，直到比被除数大
@@ -28,7 +26,7 @@ public class _29_DivedeTwoIntegers {
         }
 
         // 如果被除数在减去（多次乘以2的除数）之后仍然比原来的除数要大：亦即仍然够除
-        while (cpDividend >= Math.abs(divisor)) {
+        while (cpDividend >= Math.abs((long)divisor)) {
             // 如果不够减则证明（多次乘以2的除数）比被除数要大，则需要使除数除以2（同时c也除以2，除法用右移）
             while (cpDividend >= cpDivisor) {
                 cpDividend -= cpDivisor;
@@ -37,12 +35,17 @@ public class _29_DivedeTwoIntegers {
             cpDivisor >>= 1;
             c >>= 1;
         }
-        return (((dividend ^ divisor) >>> 31) == 1) ? (-result) : result;
+
+        result = ((((dividend ^ divisor) >>> 31) == 1) ? (-result) : result);
+
+        if ((result > Integer.MAX_VALUE) || (result < Integer.MIN_VALUE))
+            result = Integer.MAX_VALUE;
+        return (int) result;
     }
 
     public static void main(String[] args) {
         _29_DivedeTwoIntegers divedeTwoIntegers = new _29_DivedeTwoIntegers();
-        int result = divedeTwoIntegers.divide(-2147483648, -1);
+        int result = divedeTwoIntegers.divide(-1010369383, -2147483648);
         System.out.println(result);
     }
 }
