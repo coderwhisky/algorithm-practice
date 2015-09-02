990f1a2c061e53f68f56aebae629b25efac604f0
wesky260643 <260643431@qq.com>
2015-09-02 09:16:29 +0800

    ^_^

---
+++ src/main/java/org/buaa/nlp/cj/cci150/_8_4/_4_2_ExistsPath.java
@@ -0,0 +1,14 @@
+package org.buaa.nlp.cj.cci150._8_4;
+
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
+/**
+ * Created by whisky-yonk on 9/2/2015.
+ */
+public class _4_2_ExistsPath {
+
+    public static void main(String[] args) {
+        Graph graph = new Graph() {
+        }
+    }
+}
