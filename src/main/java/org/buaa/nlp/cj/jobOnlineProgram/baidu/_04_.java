b552084727f2c23c0bb26a9c8db6e2737534208e
wesky260643 <260643431@qq.com>
2015-09-30 10:01:23 +0800

    ^_^

---
+++ src/main/java/org/buaa/nlp/cj/jobOnlineProgram/baidu/_04_.java
@@ -0,0 +1,16 @@
+package org.buaa.nlp.cj.jobOnlineProgram.baidu;
+
+/**
+ * Created by Administrator on 2015/9/28.
+ */
+public class _04_ {
+//    private void f1(){
+//        System.out.println();
+//    }
+//    static final void f1() {
+//        System.out.println();
+//    }
+
+//    protected abstract void f1(){}
+    public final void f1(){}
+}
