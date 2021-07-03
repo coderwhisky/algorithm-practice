8b9ed32ccfa60df0c7318612e98143a1dfa8045e
chenjiang <chenjiang@bigo.sg>
2021-07-03 11:02:06 +0800

    add some files

---
+++ leetcode/5.最长回文子串.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-02 20:18:03
+ * @LastEditTime: 2021-07-02 20:18:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /leetcode/5.最长回文子串.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=5 lang=cpp
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        return ""
+
+    }
+};
+// @lc code=end
+
---
+++ leetcode/6.z-convert.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-02 20:21:14
+ * @LastEditTime: 2021-07-02 20:21:15
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /leetcode/6.z-convert.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=6 lang=cpp
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        return "";
+
+    }
+};
+// @lc code=end
+
